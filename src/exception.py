3899e6dec71a5ed5476d1e1d1213b9fbd9e0ff93
Singaravel <rsingrobin.learning@gmail.com>
2025-08-09 09:08:45 +0530

    Logger and exception

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,8 @@
 import sys
 import os
 import logging
+from src.logger import logging
+
 def error_message_detail(error, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()
     file_name = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
